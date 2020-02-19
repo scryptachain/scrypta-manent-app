dd7ebc9929c38a1ab2d6901c223245409c797526
turinglabsorg <turinglabs@criptext.com>
2020-02-19 14:59:41 +0100

    min fixes

--- sign_android.sh
+++ sign_android.sh
@@ -3,7 +3,6 @@
 ionic cordova build android --prod --release
 cd platforms/android
 ./gradlew bundle
-cd platforms/android/app/build/outputs/bundle/release
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ../../../../../../../keys/my-release-key.keystore app.aab alias_name
-cd platforms/android/app/build/outputs/apk/release
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ../../../../../../../keys/my-release-key.keystore app-release-unsigned.apk alias_name
+cd ..
+cd ..
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ./keys/my-release-key.keystore ./platforms/android/app/build/outputs/bundle/release/app.aab alias_name
