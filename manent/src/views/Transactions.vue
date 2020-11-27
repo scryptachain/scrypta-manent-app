9e38170199e53a2dcdb5d03911013bffb55686bd
turinglabsorg <s.cataudo@turinglabs.org>
2020-11-27 12:03:22 +0100

    removing useless component

--- manent/src/views/Transactions.vue
+++ manent/src/views/Transactions.vue
@@ -12,17 +12,15 @@
         </ion-toolbar>
       </ion-header>
       
-      <ExploreContainer name="Tab 2 page" />
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
 
 export default  {
   name: 'Tab2',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
 }
 </script>
