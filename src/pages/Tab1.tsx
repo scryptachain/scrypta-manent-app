9af01f48ba5e7a530c9a2c7df1ecbb423c4d6d1b
Steve Rogers <steve.rog02@gmail.com>
2021-01-08 16:18:30 +0100

    min fixes

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonContent, IonHeader, IonPage, IonIcon, IonBadge, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonItem } from '@ionic/react';
+import { heart, trash, star } from 'ionicons/icons';
 import './Tab1.css';
-
-
 
 const Tab1: React.FC = () => {
   return (
@@ -11,11 +9,15 @@ const Tab1: React.FC = () => {
       <IonContent fullscreen>
         <IonHeader collapse="condense">
         </IonHeader>
-        <IonCard className="cardBalance">
-          <div className="vcenter" style={{textAlign: "center"}}>
+        <IonCard className="cardBalance" style={{ height: "200px" }}>
+          <IonItem>
+            <IonBadge color="danger" slot="end">Asset</IonBadge>
+          </IonItem>
+          <div className="vcenter-dashboard" style={{ textAlign: "center" }}>
             <IonCardHeader>
               <IonCardSubtitle color="smoke">my balance</IonCardSubtitle>
               <IonCardTitle color="smoke"> 10,00 LYRA</IonCardTitle>
+              <IonCardSubtitle color="smoke">€ 5,00</IonCardSubtitle>
             </IonCardHeader>
           </div>
         </IonCard>
